3190b4d02b3764f8d07b4678988e0822cae763d9
Yves Loeys <yves.loeys@gmail.com>
2021-11-30 23:19:18 +0100

    google login firebase

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,9 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
+//import { auth } from "../../firebase";
+//import { toast } from "react-toastify";
+import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import { auth, googleAuthProvider } from "../../firebase";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
 
-const Login = () => {
+const Login = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  let dispatch = useDispatch();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await auth.signInWithEmailAndPassword(email, password);
+      const { user } = res;
+      const idTokenResult = await user.getIdTokenResult();
+      dispatch({
+        type: "LOGGED_IN_USER_REQUEST",
+        payload: {
+          email: user.email,
+          token: idTokenResult.token,
+        },
+      });
+      history.push("/");
+    } catch (error) {
+      toast.error(error.message);
+      setLoading(false);
+    }
+  };
+
+  const googleLogin = () => {
+    auth
+      .signInWithPopup(googleAuthProvider)
+      .then(async (res) => {
+        const { user } = res;
+        const idTokenResult = await user.getIdTokenResult();
+        dispatch({
+          type: "LOGGED_IN_USER_REQUEST",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+        history.push("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(error.message);
+      });
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className="form-control"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Your email"
+        autoFocus
+      />
+      <br />
+      <input
+        type="password"
+        className="form-control"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <br />
+      <Button
+        onClick={handleSubmit}
+        type="submit"
+        classname="mb-3"
+        shape="round"
+        block
+        icon={<MailOutlined />}
+        disabled={!email || password.length < 6}
+      >
+        Login with email/password
+      </Button>
+    </form>
+  );
+
   return (
-    <div>
-      <p>Login Page</p>
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          {loading ? <h4>Loading..</h4> : <h4>Login</h4>}
+
+          {loginForm()}
+          <Button
+            onClick={googleLogin}
+            type="danger"
+            classname="mb-3"
+            shape="round"
+            block
+            icon={<GoogleOutlined />}
+          >
+            Login with email/password
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -29,9 +29,10 @@ const Register = () => {
         className="form-control"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
+        placeholder="Your email"
         autoFocus
       />
-
+      <br />
       <button type="submit" className="btn btn-raised">
         Register / {email}
       </button>

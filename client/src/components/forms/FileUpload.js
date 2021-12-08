3bd98333c3f747d0bfb2d0248a29852cdbfd5f45
Yves Loeys <yves.loeys@gmail.com>
2021-12-08 22:50:49 +0100

    filupload.js

---
+++ client/src/components/forms/FileUpload.js
@@ -0,0 +1,118 @@
+import React from "react";
+import Resizer from "react-image-file-resizer";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { UploadOutlined } from "@ant-design/icons";
+import { Avatar, Badge } from "antd";
+
+const FileUpload = ({ values, setValues, setLoading }) => {
+  const { user } = useSelector((state) => ({ ...state }));
+
+  const fileUploadAndResize = (e) => {
+    // console.log(e.target.files);
+    // resize
+    let files = e.target.files; // 3
+    let allUploadedFiles = values.images;
+
+    if (files) {
+      setLoading(true);
+      for (let i = 0; i < files.length; i++) {
+        Resizer.imageFileResizer(
+          files[i],
+          720,
+          720,
+          "JPEG",
+          100,
+          0,
+          (uri) => {
+            // console.log(uri);
+            axios
+              .post(
+                `${process.env.REACT_APP_API}/uploadimages`,
+                { image: uri },
+                {
+                  headers: {
+                    authtoken: user ? user.token : "",
+                  },
+                }
+              )
+              .then((res) => {
+                console.log("IMAGE UPLOAD RES DATA", res);
+                setLoading(false);
+                allUploadedFiles.push(res.data);
+
+                setValues({ ...values, images: allUploadedFiles });
+              })
+              .catch((err) => {
+                setLoading(false);
+                console.log("CLOUDINARY UPLOAD ERR", err);
+              });
+          },
+          "base64"
+        );
+      }
+    }
+    // send back to server to upload to cloudinary
+    // set url to images[] in the parent component state - ProductCreate
+  };
+
+  const handleRemove = (public_id) => {
+    setLoading(true);
+    axios
+      .post(
+        `${process.env.REACT_APP_API}/removeImage`,
+        { public_id },
+        {
+          headers: {
+            authToken: user ? user.token : "",
+          },
+        }
+      )
+      .then((res) => {
+        setLoading(false);
+        const { images } = values;
+        let filteredImages = images.filter((item) => {
+          return item.public_id !== public_id;
+        });
+        setValues({ ...values, images: filteredImages });
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+  return (
+    <>
+      <div className="row">
+        {values.images &&
+          values.images.map((image) => (
+            <Badge
+              count="x"
+              key={image.public_id}
+              onClick={() => handleRemove(image.public_id)}
+              style={{ cursor: "pointer" }}
+            >
+              <Avatar
+                src={image.url}
+                size={100}
+                shape="square"
+                className="ml-3"
+              />
+            </Badge>
+          ))}
+        <label className="btn btn-primary">
+          <UploadOutlined /> Upload img
+          <input
+            type="file"
+            multiple
+            hidden
+            accept="images/*"
+            onChange={fileUploadAndResize}
+          />
+        </label>
+      </div>
+    </>
+  );
+};
+
+export default FileUpload;

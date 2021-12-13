9670feb08577204bcebbf9ba9fc0d21f18e4d929
Yves Loeys <yves.loeys@gmail.com>
2021-12-13 18:03:25 +0100

    creating a state and fetching products

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getProductsByCount } from "../functions/product";
+import ProductCard from "../components/cards/ProductCard";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
+  const loadAllProducts = () => {
+    setLoading(true);
+    getProductsByCount(3).then((res) => {
+      setProducts(res.data);
+      setLoading(false);
+    });
+  };
+
   return (
-    <div>
-      <p>Home Page</p>
-    </div>
+    <>
+      <div className="jumbotron">
+        {loading ? <h4>Loading...</h4> : <h4>All Products</h4>}
+      </div>
+      <div className="container">
+        <div className="row">
+          {products.map((prod) => (
+            <div className="col-md-4" key={prod._id}>
+              <ProductCard prod={prod} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
